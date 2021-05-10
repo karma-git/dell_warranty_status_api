34e9ac3c5201688a44e5cdcadc0ed37000d2d7d1
Andrei Horbach <andrewhorbach@gmail.com>
2021-05-10 05:19:27 +0300

    added secrets.ini to package

--- dell_api/__init__.py
+++ dell_api/__init__.py
@@ -3,3 +3,5 @@ This Module contains handler for Dell TechDirect APIs (Warranty Status API)
 0 APIs link https://techdirect.dell.com/portal/AboutAPIs.aspx
 1 CLI interface for visualization API data
 """
+# Version of the dell_api package
+__version__ = "1.1.0"
